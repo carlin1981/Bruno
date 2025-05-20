eb379a43167ce854180da0479fc44e1d3adc0109
ADS 2024.2N <36129382024.2N@senacrjedu.local>
2025-05-20 20:13:33 -0300

    refatoração

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,19 @@
-// src/components/Footer.js
-import React from 'react';
-import './Banner.module.css';
-
 function Banner() {
   return (
-    <banner className="Banner">
-      <p>&copy; 2025 MENGOO. Todos os direitos reservados.</p>
-    </banner>
+    <section className="banner">
+      {/* Banner principal */}
+      <div className="banner-info">
+        <h1>Stranger Things</h1>
+        <p>
+          Um grupo de crianças de uma pequena cidade precisa enfrentar forças
+          sobrenaturais.
+        </p>
+        <div className="banner-buttons">
+          <button className="button">Assistir</button>
+          <button className="button">Mais informações</button>
+        </div>
+      </div>
+    </section>
   );
 }
 
