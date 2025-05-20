2863e88619df9cae59ed2dee1a1267249da7a733
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2025-05-20 20:56:28 -0300

    save

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,8 +5,8 @@ function Banner() {
       <div className="banner-info">
         <h1>Stranger Things</h1>
         <p>
-          Um grupo de crianças de uma pequena cidade precisa enfrentar forças
-          sobrenaturais.
+        Quando um garoto desaparece, a cidade toda participa nas buscas.
+        Mas o que encontram são segredos, forças sobrenaturais e uma menina.
         </p>
         <div className="banner-buttons">
           <button className="button">Assistir</button>
